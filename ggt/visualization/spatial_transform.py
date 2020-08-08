0bd56535531b0bce68422a770caa243e2fff86da
amritrau <amritrau@gmail.com>
2020-08-08 14:30:33 -0700

    Abstract figure generation into separate function

--- ggt/visualization/spatial_transform.py
+++ ggt/visualization/spatial_transform.py
@@ -12,6 +12,38 @@ from ggt.data import FITSDataset, get_data_loader
 from ggt.models import model_factory
 from ggt.utils import discover_devices
 
+
+def visualize_spatial_transform(model, loader, outfile, nrow, device):
+    # Turn off gradients
+    with torch.no_grad():
+        # Retrieve a batch from the dataloader
+        data = next(iter(loader))[0].to(device)
+
+        # Bring the batch onto the CPU
+        in_tensor = data.cpu()
+
+        # Execute the predicted spatial transformation
+        out_tensor = model.spatial_transform(data).cpu()
+
+        # Helper function to convert a torch tensor to NumPy for plotting
+        tensor2np = lambda x: np.clip(x.numpy().transpose((1, 2, 0)), 0, 1)
+
+        # Make grids
+        in_grid = tensor2np(torchvision.utils.make_grid(
+            in_tensor[:nrow*nrow,:,:,:], nrow=nrow, pad_value=1))
+        out_grid = tensor2np(torchvision.utils.make_grid(
+            out_tensor[:nrow*nrow,:,:,:], nrow=nrow, pad_value=1))
+
+        # Plot the results side-by-side
+        plt.figure(figsize=(15,15), dpi=250)
+        plt.imshow(in_grid)
+        plt.savefig(f"{outfile}-in_grid.png")
+
+        plt.figure(figsize=(15,15), dpi=250)
+        plt.imshow(out_grid)
+        plt.savefig(f"{outfile}-out_grid.png")
+
+
 @click.command()
 @click.option('--model_path', type=click.Path(exists=True), required=True)
 @click.option('--data_dir', type=click.Path(exists=True), required=True)
@@ -46,37 +78,11 @@ def main(model_path, data_dir, split_slug, split, batch_size, nrow, n_workers,
     loader = get_data_loader(dataset,
         batch_size=batch_size, n_workers=n_workers)
 
-    # Turn off gradients
-    with torch.no_grad():
-        # Retrieve a batch from the dataloader
-        data = next(iter(loader))[0].to(device)
+    # Determine output filepath
+    outfile = model_path.replace('.pt', '')
 
-        # Bring the batch onto the CPU
-        in_tensor = data.cpu()
-
-        # Execute the predicted spatial transformation
-        out_tensor = model.spatial_transform(data).cpu()
-
-        # Helper function to convert a torch tensor to NumPy for plotting
-        tensor2np = lambda x: np.clip(x.numpy().transpose((1, 2, 0)), 0, 1)
-
-        # Make grids
-        in_grid = tensor2np(torchvision.utils.make_grid(
-            in_tensor[:nrow*nrow,:,:,:], nrow=nrow, pad_value=1))
-        out_grid = tensor2np(torchvision.utils.make_grid(
-            out_tensor[:nrow*nrow,:,:,:], nrow=nrow, pad_value=1))
-
-        # Determine output filepath
-        outfile = model_path.replace('.pt', '')
-
-        # Plot the results side-by-side
-        plt.figure(figsize=(15,15), dpi=250)
-        plt.imshow(in_grid)
-        plt.savefig(f"{outfile}-in_grid.png")
-
-        plt.figure(figsize=(15,15), dpi=250)
-        plt.imshow(out_grid)
-        plt.savefig(f"{outfile}-in_grid.png")
+    # Generate the visualizations
+    visualize_spatial_transform(model, loader, outfile, nrow, device)
 
 
 if __name__ == '__main__':
