321dd33e8092f5d85f9af12339022bc56a3ef623
Aritra Ghosh <aritraghsh09@gmail.com>
2020-10-02 18:21:19 -0400

    More verbose logging with info about each device being used logged. Helps in keeping track of runtimes of appropriate GPUs

--- ggt/utils/device_utils.py
+++ ggt/utils/device_utils.py
@@ -1,6 +1,17 @@
 import torch
-
+import logging
 
 def discover_devices():
     """Check for available devices."""
-    return "cuda" if torch.cuda.is_available() else "cpu"
+    if torch.cuda.is_available():
+
+        logging.info("Using the following GPUs")
+
+        for i in range(0,torch.cuda.device_count()):
+            logging.info(torch.cuda.get_device_name(i))
+
+        return "cuda"
+
+    else:
+        logging.info("GPU not found! Using a CPU")
+        return "cpu"
