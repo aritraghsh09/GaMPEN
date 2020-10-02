771a149319e1efddf650cbb7968a8a5f447479bd
Aritra Ghosh <aritraghsh09@gmail.com>
2020-10-02 19:38:38 -0400

    Typo corrections

--- ggt/utils/device_utils.py
+++ ggt/utils/device_utils.py
@@ -5,7 +5,7 @@ def discover_devices():
     """Check for available devices."""
     if torch.cuda.is_available():
 
-        logging.info("Using the following GPUs")
+        logging.info("Using the following GPUs:-")
 
         for i in range(0,torch.cuda.device_count()):
             logging.info(torch.cuda.get_device_name(i))
