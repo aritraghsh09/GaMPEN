934f02aec382cf001811e6b1a7f99b13a8fcd4d4
Aritra Ghosh <aritraghsh09@gmail.com>
2020-11-12 17:10:42 -0500

    Updates to make inference compatible with ggt

--- ggt/modules/inference.py
+++ ggt/modules/inference.py
@@ -13,18 +13,17 @@ from ggt.data import FITSDataset, get_data_loader
 from ggt.models import model_factory
 from ggt.utils import discover_devices
 
-
-def predict(model_path, dataset, cutout_size, channels, parallel=False,
-            batch_size=256, n_workers=1):
-    """Using the model defined in model path, return the output values for
-    the given set of images"""
+def predict(model_path, dataset, cutout_size, channels, parallel=False, \
+    batch_size=256, n_workers = 1, model_type='ggt'):
+    """Using the model defined in model path, return the output values for the given
+    set of images"""
 
     # Discover devices
     device = discover_devices()
 
     # Load the model
     logging.info("Loading model...")
-    cls = model_factory('ggt')
+    cls = model_factory(model_type)
     model = cls(cutout_size, channels)
     model = nn.DataParallel(model) if parallel else model
     model = model.to(device)
@@ -45,6 +44,9 @@ def predict(model_path, dataset, cutout_size, channels, parallel=False,
 
 
 @click.command()
+@click.option('--model_type',
+              type=click.Choice(['ggt','vgg16'], case_sensitive=False),
+              default='ggt')
 @click.option('--model_path', type=click.Path(exists=True), required=True)
 @click.option('--output_path', type=click.Path(writable=True), required=True)
 @click.option('--data_dir', type=click.Path(exists=True), required=True)
@@ -65,19 +67,22 @@ def predict(model_path, dataset, cutout_size, channels, parallel=False,
               help='''The parallel argument controls whether or not
               to use multiple GPUs when they are available''')
 @click.option('--label_col', type=str, default='bt_g')
-def main(model_path, output_path, data_dir, cutout_size, channels,
-         parallel, slug, split, normalize, batch_size, n_workers, label_col):
+@click.option('--repeat_dims/--no-repeat_dims', default=False,
+help='''In case of multi-channel data, whether to repeat a two 
+dimensional image as many times as the number of channels''')
+def main(model_path, output_path, data_dir, cutout_size, channels,\
+    parallel, slug, split, normalize, batch_size, n_workers, label_col,\
+    model_type, repeat_dims):
+    
+    # Load the data and create a dataloader
+    logging.info("Loading Images to Device")
+    dataset = FITSDataset(data_dir, slug=slug, normalize=normalize, split=split,\
+        cutout_size=cutout_size, channels = channels, label_col = label_col,\
+        repeat_dims=repeat_dims)
 
-    # Load the data and create a data loader
-    logging.info("Loading images to device...")
-    dataset = FITSDataset(data_dir, slug=slug, normalize=normalize,
-                          split=split, cutout_size=cutout_size,
-                          channels=channels, label_col=label_col)
-
-    # Make predictions
-    preds = predict(model_path, dataset, cutout_size, channels,
-                    parallel=parallel, batch_size=batch_size,
-                    n_workers=n_workers)
+    # Making Predictions
+    preds = predict(model_path, dataset, cutout_size, channels, parallel=parallel,\
+        batch_size=batch_size, n_workers = n_workers, model_type=model_type) 
 
     # Write a CSV of predictions
     catalog = pd.read_csv(Path(data_dir) / "splits/{}-{}.csv".format(slug,
