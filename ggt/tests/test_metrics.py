5e00d2ccd6c763949ea1657989217831a4bd3908
amritrau <amritrau@gmail.com>
2021-01-24 07:17:34 -0800

    Implement pairwise MAE metric

---
+++ ggt/tests/test_metrics.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from ggt.metrics import ElementwiseMae
+
+import pytest  # noqa: F401
+
+torch.manual_seed(1)
+
+
+def test_elementwise_mae():
+    metric = ElementwiseMae()
+    n_outputs = 5
+    err = torch.arange(0, n_outputs)
+
+    for i in range(100):
+        # Fake a prediction, then add a known error
+        y_pred = torch.rand(1, n_outputs)
+        y = y_pred + err
+
+        # Update the metric with this known error    
+        metric.update((y_pred, y))
+
+    # Compute the metric and confirm it's equal to the error
+    res = metric.compute()
+    assert torch.all(torch.eq(res, err))
