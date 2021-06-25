3fdc0f0d39f440ebe0234fb52824dc5ea4ed035f
Aritra Ghosh <aritraghsh09@gmail.com>
2021-06-25 16:42:31 -0400

    Bug and Style Fixes

--- ggt/tests/test_utils.py
+++ ggt/tests/test_utils.py
@@ -2,30 +2,33 @@
 
 import torch
 import torch.nn as nn
-from ggt.utils import specify_dropout_rate
+from ggt.utils import discover_devices, specify_dropout_rate
 
 import pytest  # noqa: F401
 
 torch.manual_seed(1)
 
+
 def test_specify_dropout_rate():
 
     # Defining a basic model
     model = nn.Sequential(
-       nn.Dropout(0.5),
-       nn.Linear(2048, 1024),
-       nn.ReLU(inplace=True),
-       nn.Dropout(0.5),
-       nn.Linear(1024,512),
-       nn.ReLU(inplace=True),
-       nn.Linear(512,1)
+        nn.Dropout(0.5),
+        nn.Linear(2048, 1024),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.5),
+        nn.Linear(1024, 512),
+        nn.ReLU(inplace=True),
+        nn.Linear(512, 1),
     )
+
+    model = model.to(discover_devices())
 
     # Picking a random dropout rate to set
     dropout_rate = torch.rand(1)
 
     # Setting the dropout rates
-    specify_dropout_rate(model,float(dropout_rate))
+    specify_dropout_rate(model, float(dropout_rate))
 
     # Storing the Dropout rates from the model in an array
     model_dropout_rates = []
@@ -34,5 +37,3 @@ def test_specify_dropout_rate():
             model_dropout_rates.append(m.p)
 
     assert torch.all(torch.eq(dropout_rate, torch.tensor(model_dropout_rates)))
-
-
