46cc5dcbe86877b04be4871fb6c86997752bdebc
Aritra Ghosh <aritraghsh09@gmail.com>
2021-10-16 01:30:17 -0400

    Updating STN Framework to be compatible with new models

--- ggt/visualization/spatial_transform.py
+++ ggt/visualization/spatial_transform.py
@@ -72,7 +72,18 @@ def visualize_spatial_transform(
 
 
 @click.command()
+@click.option(
+    "--model_type",
+    type=click.Choice(
+        ["ggt", "vgg16", "ggt_no_gconv", "vgg16_w_stn", "vgg16_w_stn_drp"],
+        case_sensitive=False,
+    ),
+    default="ggt",
+)
 @click.option("--model_path", type=click.Path(exists=True), required=True)
+@click.option("--cutout_size", type=int, default=167)
+@click.option("--channels", type=int, default=1)
+@click.option("--n_out", type=int, default=1)
 @click.option("--data_dir", type=click.Path(exists=True), required=True)
 @click.option("--split_slug", type=str, required=True)
 @click.option(
@@ -83,7 +94,11 @@ def visualize_spatial_transform(
 @click.option("--n_workers", type=int, default=8)
 @click.option("--normalize/--no-normalize", default=True)
 def main(
+    model_type,
     model_path,
+    cutout_size,
+    channels,
+    n_out,
     data_dir,
     split_slug,
     split,
@@ -100,8 +115,17 @@ def main(
     device = discover_devices()
 
     # Create the model given model_type
-    cls = model_factory("ggt")  # TODO @amritrau This can be cleaner
-    model = cls()
+    cls = model_factory(model_type)
+    model_args = {
+        "cutout_size": cutout_size,
+        "channels": channels,
+        "n_out": n_out,
+    }
+
+    if model_type == "vgg16_w_stn_drp":
+        model_args["dropout"] = "True"
+
+    model = cls(**model_args)
     model = model.to(device)
 
     # Load the model from a saved state if provided
