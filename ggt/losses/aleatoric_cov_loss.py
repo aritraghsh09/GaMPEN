6f775381ae296348fb75fb561f5f071fba43786e
Aritra Ghosh <aritraghsh09@gmail.com>
2021-11-29 20:22:33 -0500

    Testing out an aleatoric loss with the funll covariance matrix

---
+++ ggt/losses/aleatoric_cov_loss.py
@@ -0,0 +1,38 @@
+import torch
+
+
+def aleatoric_cov_loss(outputs, targets, average=True):
+    """
+    Computes the Aleatoric Loss while including the full
+    covariance matrix of the outputs.
+
+    Args:
+        outputs: (tensor) - predicted outputs from the model
+        targets: (tensor) - ground truth labels
+        size_average: (bool) - if True, the losses are
+               averaged over all elements of the batch
+    Returns:
+        aleatoric_loss: (tensor) - aleatoric loss
+    """
+
+    num_out = outputs.shape[len(outputs.shape) - 1]
+    if num_out % 2 != 0:
+        raise ValueError(
+            "The number of predicted variables should be divisible by "
+            "2 for calculation of aleatoric loss"
+        )
+
+    y_hat = outputs[..., :int(num_out / 2)]
+    s_k = outputs[..., -int(num_out / 2):]
+
+    # Compute the aleatoric loss
+    aleatoric_loss = (
+        0.5 * torch.pow(y_hat - targets, 2) * torch.exp(-1.0 * s_k) + 0.5 * s_k
+    )
+
+    if average:
+        aleatoric_loss = torch.mean(aleatoric_loss)
+    else:
+        aleatoric_loss = torch.sum(aleatoric_loss)
+
+    return aleatoric_loss
--- ggt/losses/losses.py
+++ ggt/losses/losses.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 
 from .aleatoric_loss import aleatoric_loss
+from .aleatoric_cov_loss import aleatoric_cov_loss
 
 
 class AleatoricLoss(nn.Module):
@@ -10,3 +11,12 @@ class AleatoricLoss(nn.Module):
 
     def forward(self, outputs, targets):
         return aleatoric_loss(outputs, targets, average=self.average)
+
+
+class AleatoricCovLoss(nn.Module):
+    def __init__(self, average=True):
+        super(AleatoricCovLoss, self).__init__()
+        self.average = average
+
+    def forward(self, outputs, targets):
+        return aleatoric_cov_loss(outputs, targets, average=self.average)
