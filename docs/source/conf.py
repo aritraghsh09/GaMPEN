5c3e32a140fa947bb899c2f44161b985c5a53343
Aritra Ghosh <aritraghsh09@gmail.com>
2022-11-08 17:32:53 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,21 +36,6 @@ extensions = [
     'autoapi.extension',
 ]
 
-myst_enable_extensions = [
-    "amsmath",
-    "colon_fence",
-    "deflist",
-    "dollarmath",
-    "fieldlist",
-    "html_admonition",
-    "html_image",
-    "linkify",
-    "replacements",
-    "smartquotes",
-    "strikethrough",
-    "substitution",
-    "tasklist",
-]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
