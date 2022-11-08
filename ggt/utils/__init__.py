7db42d7be866b6de0170109457543d9ee5037793
Aritra Ghosh <aritraghsh09@gmail.com>
2022-11-08 14:40:00 -0500

    Fixing import line

--- ggt/utils/__init__.py
+++ ggt/utils/__init__.py
@@ -8,7 +8,7 @@ from .tensor_utils import (
     metric_output_transform_al_cov_loss,
 )
 from .data_utils import load_cat
-from .model_utils import get_output_shape, enable_dropout
+from .model_utils import get_output_shape, enable_dropout, specify_dropout_rate
 
 __all__ = [
     "discover_devices",
