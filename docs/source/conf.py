b5da4584f03208a70852679c0035ebf2eb8383bc
Aritra Ghosh <aritraghsh09@gmail.com>
2022-12-17 17:51:30 -0500

    adding mathjax

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'GaMPEN'
-copyright = '2022, Aritra Ghosh, Amrit Rau, Aayush Mishra'
-author = 'Aritra Ghosh, Amrit Rau, Aayush Mishra'
+project = "GaMPEN"
+copyright = "2022, Aritra Ghosh, Amrit Rau, Aayush Mishra"
+author = "Aritra Ghosh, Amrit Rau, Aayush Mishra"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,16 +31,16 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
-    'myst_parser',
-    'autoapi.extension',
-    'sphinx.ext.mathjax',
+    "sphinx_rtd_theme",
+    "myst_parser",
+    "autoapi.extension",
+    "sphinx.ext.mathjax",
 ]
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-autoapi_dirs = ['../../ggt']
+templates_path = ["_templates"]
+autoapi_dirs = ["../../ggt"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -53,9 +53,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
