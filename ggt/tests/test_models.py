0783692332f7ddfbcc8dd9f983fe096e4aaff7d6
Aritra Ghosh <aritraghsh09@gmail.com>
2025-03-20 23:47:04 -0700

    Removing GGT tests

--- ggt/tests/test_models.py
+++ ggt/tests/test_models.py
@@ -8,18 +8,6 @@ import pytest  # noqa: F401
 torch.manual_seed(1)
 
 
-def test_ggt():
-    cls = model_factory("ggt")
-    model = cls(169, 1, 1)
-    print(model)
-
-
-def test_ggt_no_gconv():
-    cls = model_factory("ggt_no_gconv")
-    model = cls(169, 1, 1)
-    print(model)
-
-
 def test_vgg16():
     cls = model_factory("vgg16")
     model = cls(169, 1, 1, pretrained=False)
